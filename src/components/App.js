8e8a847d6771fa9bfbefad02d5b65efa2b817d2a
Zeki Unyildiz <zekiunyildiz@gmail.com>
2021-03-18 20:02:02 +0300

    Multiple Context

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import BookList from "./BookList";
 import BookContextProvider from "../contexts/BookContext";
+import ThemeContextProvider from "../contexts/ThemeContext";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <ThemeContextProvider>
         <BookContextProvider>
           <BookList />
         </BookContextProvider>
-      </div>
+      </ThemeContextProvider>
     );
   }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,31 +3,42 @@ import Book from "./Book";
 import "./BookList.css";
 import "../contexts/BookContext";
 import { BookContext } from "../contexts/BookContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class BookList extends Component {
   render() {
     return (
-      <BookContext.Consumer>
-        {(value) => {
-          return (
-            <section className="page-section bg-light" id="portfolio">
-              <div className="container">
-                <div className="text-center">
-                  <h2 className="section-heading text-uppercase">Bookshelf</h2>
-                  <h3 className="section-subheading text-muted">
-                    Lorem ipsum dolor sit amet consectetur.
-                  </h3>
-                </div>
-                <div className="row">
-                  {value.books.map((book, i) => {
-                    return <Book book={book} key={i} />;
-                  })}
-                </div>
-              </div>
-            </section>
-          );
-        }}
-      </BookContext.Consumer>
+      <ThemeContext.Consumer>
+        {(contextTheme) => (
+          <BookContext.Consumer>
+            {(contextBook) => {
+              const { books } = contextBook;
+              const { isDarkTheme, dark, light } = contextTheme;
+              const theme = isDarkTheme ? dark : light;
+
+              return (
+                <section className="page-section" style={{background:theme.bg, color:theme.txt}} id="portfolio">
+                  <div className="container">
+                    <div className="text-center">
+                      <h2 className="section-heading text-uppercase">
+                        Bookshelf
+                      </h2>
+                      <h3 className="section-subheading text-muted">
+                        Lorem ipsum dolor sit amet consectetur.
+                      </h3>
+                    </div>
+                    <div className="row">
+                      {books.map((book, i) => {
+                        return <Book book={book} key={i} />;
+                      })}
+                    </div>
+                  </div>
+                </section>
+              );
+            }}
+          </BookContext.Consumer>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
